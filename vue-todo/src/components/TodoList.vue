1f358a33de9de218967618f62936f0668fd879a9
highcloud100 <highcloud100@inha.edu>
2022-01-17 20:44:50 +0900

    day6

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
     <section>
-       <ul>
-           <li v-for = "(todoItem,index) in propsdata" class="shadow" v-bind:key="todoItem">
+        <transition-group name="list" tag="ul"> <!--애니메이션 추가할때 사용하는 태그-->
+           <li v-for = "(todoItem,index) in propsdata" class="shadow" v-bind:key="todoItem" >
                 <i class="checkBtn fa fa-check" aria-hidden="true"></i>
                 {{todoItem}}
                 <span class="removeBtn" type="button" v-on:click="removeTodo(todoItem,index)">
                     <i class="fa fa-trash-alt" aria-hidden="true"></i>
                 </span>
            </li>
-       </ul>
+        </transition-group>
     </section>
 </template>
 
@@ -25,6 +25,20 @@ export default {
 </script>
 
 <style>
+    .list-item{
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .list-move{
+        transition: transform 1s;
+    }
+    .list-enter-active, .list-leave-active{
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to{
+        opacity: 0;
+        transform: translateY(30px)
+    }
     ul{
         list-style-type: none;
         padding-left: 0px;
